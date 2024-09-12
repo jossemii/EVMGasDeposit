762a2771c3356e6137fbcad15b03dc98a644762c
0xf965 <0xf965@non.exists>
2024-09-12 13:29:17 +0200

    added nodo payment system interface

---
+++ node_payment_system_interface/ethereum/envs.py
@@ -0,0 +1,7 @@
+ETH_PROVIDER = ""
+
+ETH_LEDGER = "mainnet"
+
+PARITY_FACTOR = 100
+# Factor de paridad entre el valor de gas (representación de los recursos del nodo)
+# y el activo de la red de procesamiento de pagos.
